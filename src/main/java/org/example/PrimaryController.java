0e43f036efb2bf1523442cb38d3ba301016325d3
Jorge Lopez <jorgeluislopezperez.sanjose@alumnado.fundacionloyola.net>
2022-10-23 19:01:53 +0200

    Ejercicio actualizado

--- src/main/java/org/example/PrimaryController.java
+++ src/main/java/org/example/PrimaryController.java
@@ -1,12 +1,33 @@
 package org.example;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
 
-public class PrimaryController {
+public class PrimaryController implements Initializable {
 
+	@FXML
+	private TextField nameTextField;
+	
+	@FXML
+	private TextField signTextField;
+	
     @FXML
-    private void switchToSecondary() throws IOException {
+    private void displayFortune() throws IOException {
+    	StaticData.latestName = nameTextField.getText();
+    	StaticData.latestSign = signTextField.getText();
         App.setRoot("secondary");
     }
+
+    //La interfaz Initializable de javafx permite declarar un método para que se ejecute
+    //cada vez que se cambie a la escena de este controlador sin tener que modificar App.java.
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		nameTextField.setText(StaticData.latestName);
+		signTextField.setText(StaticData.latestSign);
+	}
 }
--- src/main/java/org/example/SecondaryController.java
+++ src/main/java/org/example/SecondaryController.java
@@ -1,12 +1,39 @@
 package org.example;
 
 import java.io.IOException;
+import java.net.URL;
+import java.util.ResourceBundle;
+
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
 
-public class SecondaryController {
+public class SecondaryController implements Initializable{
 
+	@FXML
+	private Label fortuneDisplayLabel;
+	
     @FXML
-    private void switchToPrimary() throws IOException {
+    private void goBack() throws IOException {
         App.setRoot("primary");
     }
+
+    //La interfaz Initializable de javafx permite declarar un método para que se ejecute
+    //cada vez que se cambie a la escena de este controlador sin tener que modificar App.java.
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		String fortune = StaticData.getRandomFortune();
+		String luckyNumbers = drawLuckyNumbers();
+		String fullFortune = StaticData.latestSign + ", " + fortune + "\nLos números de la suerte para " + StaticData.latestName + " son:\n" + luckyNumbers;
+		fortuneDisplayLabel.setText(fullFortune);
+	}
+
+	private String drawLuckyNumbers() {
+		String luckyNumbers = "";
+		
+		for (int i = 0; i<7; i++) {
+			luckyNumbers += (int)Math.floor(Math.random() * 100) + " ";
+		}
+		return luckyNumbers;
+	}
 }
